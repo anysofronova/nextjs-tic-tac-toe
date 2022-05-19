5e89c1bb35b58cbfe1188e7ebb6da4281ae05dde
Anna <anysofronova@yandex.ru>
2022-05-19 12:06:53 +0300

    Main board

---
+++ components/Square.tsx
@@ -0,0 +1,32 @@
+import styles from "./Square.module.css";
+import cn from "classnames";
+type typePlayer = "X" | "O" | null;
+
+const Square = ({
+  winner,
+  onClick,
+  value,
+}: {
+  winner: typePlayer;
+  value: typePlayer;
+  onClick: () => void;
+}) => {
+  if (!value)
+    return (
+      <button
+        onClick={onClick}
+        disabled={Boolean(winner)}
+        className={cn(styles.square)}
+      />
+    );
+  return (
+    <button
+      className={cn(styles.square, value === "X" ? styles.x : styles.y)}
+      disabled
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
